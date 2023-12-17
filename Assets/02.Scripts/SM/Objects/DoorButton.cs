853dcb9ed6a08398cdb149c37e14b049dad58391
Lawrence1031h <lawrence1031h@gmail.com>
2023-12-17 23:11:00 +0900

    20231217_중간저장
    23:10
    문과 상호작용 만들기

--- Assets/02.Scripts/SM/Objects/DoorButton.cs
+++ Assets/02.Scripts/SM/Objects/DoorButton.cs
@@ -7,6 +7,9 @@ public class DoorButton : MonoBehaviour
     public GameObject button;
     public GameObject door;
 
+    /// <summary>
+    /// 문을 열 수 있는 버튼의 능력을 추가하려고 합니다.
+    /// </summary>
     public void PushDoorButton()
     {
         door.SetActive(false);        
---
+++ Assets/02.Scripts/SM/Objects/DoorController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorController : MonoBehaviour
+{
+    private bool isOpen = false;
+    public GameObject Door;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ToggleDoor();
+        }
+    }
+
+    /// <summary>
+    /// 키가 있다면 문을 여는 기믹이 시작되게 해야합니다.
+    /// </summary>
+    private void ToggleDoor()
+    {
+        //if (hasKey)
+        //{
+        //    isOpen = true;
+        //    transform.localScale = new Vector3(3, 3, 1);
+        //    transform.position = new Vector3(0, 3f, 50);
+        //}
+        //else
+        //{
+        //    Debug.Log("You need a Key. Find the Key in this room");
+        //}
+
+        if (isOpen)
+        {
+            transform.localScale = new Vector3(3, 3, 1);
+            transform.position = new Vector3(0, 3f, 50);
+            //Door.SetActive(false);
+        }
+        else
+        {
+            transform.localScale = new Vector3(10, 8, 1);
+            transform.position = new Vector3(0, 3f, 50);
+            //Door.SetActive(true);
+        }
+
+        isOpen = !isOpen;
+    }
+}
