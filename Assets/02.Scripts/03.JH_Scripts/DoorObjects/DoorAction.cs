ea90ddd0e2bb8c51da4bdee672ffe09d457d8a38
Lawrence1031h <lawrence1031h@gmail.com>
2023-12-18 12:49:13 +0900

    20231218_DoorOpen
    12:48 DoorAction Script 생성
    Door Open 관련 스크립트 작성중

---
+++ Assets/02.Scripts/03.JH_Scripts/DoorObjects/DoorAction.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class DoorAction : MonoBehaviour
+{
+    public GameObject Door;
+    private Position doorPosition;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+        }
+    }
+}
