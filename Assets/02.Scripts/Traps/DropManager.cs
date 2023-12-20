fddea1c3549c8041b493ab1315e91dc124e84e41
joominji <jmj00179@naver.com>
2023-12-20 22:12:40 +0900

    20231220 - Cube 설치

--- Assets/02.Scripts/Traps/DropManager.cs
+++ Assets/02.Scripts/Traps/DropManager.cs
@@ -10,15 +10,21 @@ public class DropManager : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         // 'Player' 태그를 가진 오브젝트가 트리거에 닿았는지 확인.
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Cube"))
         {
             // 플레이어가 함정에 닿으면 콘솔에 메시지 출력
             Debug.Log("함정 발동");
+            Debug.Log("dropPrefab.transform.position" + dropPrefab.transform.position);
 
             // 드랍 프리팹을 프리팹 자체의 위치에 생성.
             // Instantiate 함수는 새 게임 오브젝트를 생성하고, 이를 게임 월드에 배치함.
             // dropPrefab.transform.position은 프리팹의 위치를 참조함.
             // Quaternion.identity는 회전을 적용하지 않음을 나타냄.
+            Instantiate(dropPrefab, dropPrefab.transform.position, Quaternion.identity);
+            
+        }
+        else if (other.CompareTag("Player"))
+        {
             Instantiate(dropPrefab, dropPrefab.transform.position, Quaternion.identity);
         }
     }
