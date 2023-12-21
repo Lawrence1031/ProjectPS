8c39a2e46d650e05477aaeaefafab45970542161
junhyeongBak <dj043045@gmail.com>
2023-12-21 10:33:48 +0900

    Edit

---
+++ Assets/02.Scripts/CheckPoint/EndCheck.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EndCheck : MonoBehaviour
+{
+    public GameObject ClearWindow;
+    public TextMeshProUGUI ClearText;
+
+    //private void Awake()
+    //{
+    //    ClearWindow.SetActive(false);
+    //}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ClearWindow.SetActive(true);
+            Time.timeScale = 0;
+
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+}
---
+++ Assets/02.Scripts/Items/Cube/CubeObject.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeObject : MonoBehaviour, IInteraction
+{
+    public HintData hintData;
+    bool isRolling;
+    public float rotationSpeed = 5;
+    public GameObject Player;
+    public GameObject Cube;
+    bool mathed;
+
+    Bounds bound;
+    Vector3 left, right, up, down;
+
+    void Start()
+    {
+        bound = GetComponent<BoxCollider>().bounds;
+      
+        right = new Vector3(bound.size.x / 2, -bound.size.y / 2, 0);
+        up = new Vector3(0, -bound.size.y / 2, bound.size.z / 2);
+      
+    }
+
+    public string GetInteractPrompt()
+    {
+        return string.Format("{0}", hintData.displayName);
+    }
+    public void OnInteract()
+    {
+       
+            if (Input.GetKey(KeyCode.R) && !isRolling)
+            {
+                StartCoroutine(Roll(up));
+            }
+      
+        else if (Input.GetKey(KeyCode.T) && !isRolling)
+        {
+            StartCoroutine(Roll(right));
+        }
+    
+    }
+
+
+    IEnumerator Roll(Vector3 positionToRotation)
+    {
+        isRolling = true;
+        float angle = 0;
+        Vector3 point = transform.position + positionToRotation;
+        Vector3 axis = Vector3.Cross(Vector3.up, positionToRotation).normalized;
+
+        while (angle < 90f)
+        {
+            float angleSpeed = Time.deltaTime + rotationSpeed;
+            transform.RotateAround(point, axis, angleSpeed);
+            angle += angleSpeed;
+            yield return null;
+        }
+
+        transform.RotateAround(point, axis, 90 - angle);
+        isRolling = false;
+    }
+
+    public string GetInteratHint()
+    {
+        throw new System.NotImplementedException();
+    }
+}
